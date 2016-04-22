30c2f7f56a7a49aca2bcfd100117993aa4bc9150
dhwanish <ds2246@cornell.edu>
2016-04-22 12:44:34 -0400

    changed button text and number of reviews part in views.py

--- mysite/views.py
+++ mysite/views.py
@@ -6,12 +6,12 @@ from find import ReviewFinder
 def reviews(request):
     city = request.GET.get('city')
     f = ReviewFinder(city)
-    return render_to_response('./reviews.html', {'city': city, 'reviews': f.find_reviews(str(request.GET.get('keywords')), 5)});
+    return render_to_response('./reviews.html', {'city': city, 'reviews': f.find_reviews(str(request.GET.get('keywords')), 20)});
 
 def moreReviews(request):
     city = request.GET.get('city')
     f = ReviewFinder(city)
-    return render_to_response('./moreReviews.html', {'city': city, 'reviews': f.find_more(str(request.GET.get('review_id')), 5) })
+    return render_to_response('./moreReviews.html', {'city': city, 'reviews': f.find_more(str(request.GET.get('review_id')), 20) })
 
 def home(request):
     return render_to_response('./index.html');
@@ -19,4 +19,4 @@ def home(request):
 def showBusinesses(request):
 	city = request.GET.get('city')
 	f = ReviewFinder(city)
-	return render_to_response('./showBusinesses.html',{'city':city, 'businesses': f.find_businesses(str(request.GET.get('review_id')),str(request.GET.get('business_id')),5)})
+	return render_to_response('./showBusinesses.html',{'city':city, 'businesses': f.find_businesses(str(request.GET.get('review_id')),str(request.GET.get('business_id')),20)})
