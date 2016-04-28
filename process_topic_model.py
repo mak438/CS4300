831868bf78841a934c3fbf0469ae6f3c96aa6a28
Moshe Klebanov <moshe_klebanov@yahoo.com>
2016-04-27 21:57:30 -0600

    Added topic model datafile

--- process_topic_model.py
+++ process_topic_model.py
@@ -1,44 +1,42 @@
 city = 'Urbana'
 
-import collections
-import shelve
+import re
 import pickle
-import json
+import shelve
 import semidbm
+import pandas
 from os.path import basename
+from collections import defaultdict
 
-db = collections.defaultdict(list)
+tokenize_regex = re.compile(r'[A-Za-z0-9]+')
 
-businesses_to_collect = set()
-
-with open('Business.json') as f:
-    businesses = {b['business_id'].encode('utf-8'): b['name'].encode('utf-8') for b in json.load(f)}
-
-with open(city + '.keys.txt') as keys:
-    for row in keys:
-        row = row.split()
-        topic = row.pop(0)
-        row.pop(0) # Ignore this
-        for term in row:
-            db["t=" + term].append((int(topic), 1))
-
-with open(city + '.topics.txt') as topics:
-    for row in topics:
-        row = row.split()
-        row.pop(0) # Ignore this
-        review = basename(row.pop(0))
-        while len(row)>0:
-            topic = row.pop(0)
-            weight = row.pop(0)
-            db["c=" + topic].append((review, float(weight)))
-
-with open(city + '.json') as reviews:
-    reviews_list = json.load(reviews)
-    for row in reviews_list:
-        db["r=" + row['review_id'].encode('utf-8')] = (row['text'].encode('utf-8'), row['business_id'].encode('utf-8'))
-        businesses_to_collect.add(row['business_id'].encode('utf-8'))
+businesses = pandas.read_json('Business.json')
+topics = pandas.melt(pandas.read_table('Urbana.topics.txt', header=None, names=['seq', 'review_id'] + range(1,100), index_col=False), id_vars=['seq', 'review_id'], var_name='topic', value_name='weight')
+topics.review_id = topics.review_id.apply(basename)
+wordweights = pandas.read_table('Urbana.wordweights.txt', header=None, names=['topic', 'term', 'weight'])
+reviews = pandas.read_json('Urbana.json')
 
 s = shelve.Shelf(semidbm.open(city + '.db', flag='n'), protocol=pickle.HIGHEST_PROTOCOL)
-for k,v in db.items():
+
+for b in businesses.itertuples():
+    s["b=" + str(b.business_id)] = (b.name, b.categories, b.stars)
+print("Written businesses")
+
+categories = defaultdict(list)
+for c in topics.itertuples():
+    categories["c=" + str(c.topic)].append((c.review_id, c.weight))
+for k,v in categories.items():
     s[k] = v
+print("Written categories")
+
+term_weights = defaultdict(list)
+for t in wordweights.itertuples():
+    term_weights["t=" + str(t.term)].append((t.topic, t.weight))
+for k, v in term_weights.items():
+    s[k] = v
+print("Written terms")
+
+for r in reviews.itertuples():
+    s["r=" + str(r.review_id)] = (r.text, r.business_id, r.stars, r.date)
+print("Written reviews")
 s.close()
