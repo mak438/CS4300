1fc836f1830094d06f6c5339809f75ea4d167252
Moshe Klebanov <moshe_klebanov@yahoo.com>
2016-05-02 19:51:14 -0600

    Removed Restaurants from topic model

--- process_topic_model.py
+++ process_topic_model.py
@@ -7,6 +7,7 @@ import semidbm
 import pandas
 from os.path import basename
 from collections import defaultdict
+from operator import itemgetter
 from math import sqrt
 
 NUM_TOPICS = 40
@@ -15,34 +16,38 @@ tokenize_regex = re.compile(r'[A-Za-z]+')
 
 s = shelve.Shelf(semidbm.open(city + '.db', flag='n'), protocol=pickle.HIGHEST_PROTOCOL)
 for b in pandas.read_json('Business.json').itertuples():
-    s["b=" + str(b.business_id)] = (b.name, b.categories, b.stars)
+    s["b=" + str(b.business_id)] = (b.name, [cat for cat in b.categories if cat != 'Restaurants'], b.stars)
 
 reviews_body = pandas.read_json('Urbana.json')
 
 reviews_by_term = defaultdict(list)
+term_frequencies = defaultdict(int)
 for r in reviews_body.itertuples():
     for term in tokenize_regex.findall(r.text):
         reviews_by_term[term].append((r.review_id, 1))
+        term_frequencies[term] += 1
 
 for term, reviews in reviews_by_term.items():
-    s["t=" + term] = reviews
+    s["t=" + term] = (reviews, term_frequencies[term])
 
 topic_weights = pandas.read_table('Urbana.keys.txt', header=None, names=['topic', 'topic_weight'], usecols=[0,1])
 topics = pandas.melt(pandas.read_table('Urbana.topics.txt', header=None, names=['seq', 'review_id'] + range(1,NUM_TOPICS), index_col=False), id_vars=['seq', 'review_id'], var_name='topic', value_name='weight')
 topics.review_id = topics.review_id.apply(basename)
 topics = topics.merge(topic_weights, on='topic')
 
+topic_assignments = {t.topic: (t.title, t.color) for t in pandas.read_table('topics.txt', sep='-', header=None, names=['topic', 'title', 'color']).itertuples()}
+
 topics_by_review = defaultdict(list)
 reviews_by_topic = defaultdict(list)
 for entry in topics.itertuples():
     topics_by_review[entry.review_id].append((entry.topic, entry.weight * sqrt(entry.topic_weight)))
     reviews_by_topic[entry.topic].append((entry.review_id, entry.weight * sqrt(entry.topic_weight)))
-    
+
 for review_id, topics in topics_by_review.items():
     s["rt=" + review_id] = topics
 for review in reviews_body.itertuples():
     s["r=" + review.review_id] = (review.text, review.business_id, review.stars)
 for topic, reviews in reviews_by_topic.items():
-    s["c=" + str(topic)] = reviews
+    s["c=" + str(topic)] = (sorted(reviews, key=itemgetter(1), reverse=True), topic_assignments[topic])
 
 s.close()
