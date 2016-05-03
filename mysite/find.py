29696ecd3f6a997fc253b0f25b446f853bb1f374
dhwanish <ds2246@cornell.edu>
2016-05-02 21:34:19 -0400

    fixed bug in find.py for .items and show more business page

--- mysite/find.py
+++ mysite/find.py
@@ -123,7 +123,7 @@ class ReviewFinder:
                                                                                        text=props[1],
                                                                                        stars=tuple([True] * props[3] + [False] * (5-props[3])),
                                                                                        business=self.__business(props[2]),
-                                                                                       topics=self.__top_topics(review_id)[:5]) for review_id, props in reviews.items()]
+                                                                                       topics=self.__top_topics(review_id)[:5]) for review_id, props in reviews]
     
     def find_by_topic(self, topic, limit=None):
         
